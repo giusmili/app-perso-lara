ffe18371e882f27fbad9c7ef231543885c69be1f
giusmili <giusmili67@gmail.com>
2025-04-07 18:39:07 +0200

    css

--- resources/views/membres/index.blade.php
+++ resources/views/membres/index.blade.php
@@ -5,44 +5,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Membre</title>
-    {{-- <link rel="stylesheet" href="{{ Vite::asset('resources/css/app.css') }}"> --}}
+    <link rel="stylesheet" href="{{ Vite::asset('resources/css/app.css') }}">
 </head>
 <body>
-    <h2>Liste des membres</h2>
-
-    @if (session('success'))
+    <header>
+        <h1>Liste des membres</h1>
+    </header>
+    <p>
+        @if (session('success'))
         <div style="color: green;">{{ session('success') }}</div>
     @endif
-
-    <table border="1" cellpadding="8" cellspacing="0">
-        <thead>
-            <tr>
-                <th>Nom</th>
-                <th>Prénom</th>
-                <th>Email</th>
-                <th>Ville</th>
-                <th>Pays</th>
-                <th>Date</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($membres as $membre)
+    </p>
+<main>
+    <section class="tables-user">
+        <table>
+            <thead>
                 <tr>
-                    <td>{{ $membre->nom }}</td>
-                    <td>{{ $membre->prenom }}</td>
-                    <td>{{ $membre->email }}</td>
-                    <td>{{ $membre->ville }}</td>
-                    <td>{{ $membre->pays }}</td>
-                    <td>{{ $membre->date }}</td>
-                    <td>
-                       
-                        <a href="{{ route('membres.edit', $membre->id) }}">Modifier</a>
-                        {{-- On ajoutera la suppression ici plus tard --}}
-                    </td>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Email</th>
+                    <th>Ville</th>
+                    <th>Pays</th>
+                    <th>Date</th>
+                    <th>Actions</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($membres as $membre)
+                    <tr>
+                        <td>{{ $membre->nom }}</td>
+                        <td>{{ $membre->prenom }}</td>
+                        <td>{{ $membre->email }}</td>
+                        <td>{{ $membre->ville }}</td>
+                        <td>{{ $membre->pays }}</td>
+                        <td>{{ $membre->date }}</td>
+                        <td>
+                        
+                            <a href="{{ route('membres.edit', $membre->id) }}">Modifier</a>
+                            {{-- On ajoutera la suppression ici plus tard --}}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <a href="{{ url("/dashboard")}}" class="btn-default btn-success">Dashboard</a>
+    </section>
+</main>
+    <footer>
+        <p>
+            &copy; - MIT - 2025
+        </p>
+    </footer>
 </body>
 </html>
